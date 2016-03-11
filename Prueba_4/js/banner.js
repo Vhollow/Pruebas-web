5332540a8d558ed98cc593db95b63e1ca1418c4c
Vicente Martínez <vicentemartinezf@gmail.com>
2016-03-11 11:35:16 +0100

    Cuarta prueba
    Banner javascript

---
+++ Prueba_4/js/banner.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//creo array de imágenes 
+var array_imagen;
+array_imagen = new Array(4) ;
+array_imagen[0] = new Image(120,41) ;
+array_imagen[0].src = "Imagenes/img1.jpg" ;
+array_imagen[1] = new Image(120,41) ;
+array_imagen[1].src = "Imagenes/img2.jpg" ;
+array_imagen[2] = new Image(120,41) ;
+array_imagen[2].src = "Imagenes/img3.png"; 
+array_imagen[3] = new Image(120,41) ;
+array_imagen[3].src = "Imagenes/img4.jpg";
+
+//variable para llevar la cuenta de la imagen siguiente 
+contador = 0 ;
+
+//función para rotar el banner 
+function alternar_banner(){ 
+   	window.document["banner"].src = array_imagen[contador].src ;
+   	contador ++ ;
+   	contador = contador % array_imagen.length ;
+   	setTimeout("alternar_banner()",1000) ;
+}
+
+
